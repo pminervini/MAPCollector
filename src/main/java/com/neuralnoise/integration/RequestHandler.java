6353e74e3f0f3aa003e51e9bf11d9e2ae9516f89
pminervini <p.minervini@gmail.com>
2014-06-01 01:22:16 +0200

    events are transmitted via an AMQ channel

--- src/main/java/com/neuralnoise/integration/RequestHandler.java
+++ src/main/java/com/neuralnoise/integration/RequestHandler.java
@@ -1,9 +1,15 @@
 package com.neuralnoise.integration;
 
+import java.util.List;
+
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.stereotype.Component;
 
+import com.google.common.collect.Lists;
+import com.neuralnoise.integration.geo.Location;
+import com.neuralnoise.integration.geo.Point;
 import com.neuralnoise.integration.util.CAnswer;
+import com.neuralnoise.integration.util.CEvent;
 import com.neuralnoise.integration.util.CRequest;
 
 @Component
@@ -15,6 +21,22 @@ public class RequestHandler {
 		
 		CAnswer answer = new CAnswer();
 		answer.setRequest(request);
+		
+		List<CEvent> events = Lists.newLinkedList();
+		
+		CEvent event = new CEvent();
+		event.setName("Event1");
+		event.setContent("This is a sample Event");
+		
+		Location location = new Location();
+		location.setName("A Street, B");
+		location.setPoint(new Point(11.0, 12.0));
+		
+		event.setLocation(location);
+		
+		events.add(event);
+		
+		answer.setEvents(events);
 		
 		return answer;
 	}
